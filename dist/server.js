231f2c5760c8ae1c23604829cb512c8b6ca1b0f5
Ben Remmer <Ben.Remmer@softwire.com>
2024-07-01 13:57:16 +0100

    added back end functionality, with next 5 buses at nearest 2 bus stops

--- dist/server.js
+++ dist/server.js
@@ -18,17 +18,57 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send("Hello I am your express server :)");
     const name = req.query.name;
-    fetchWithAwait(name);
+    postCodeToFirst5BusesAt2NearestBusStops("SW15 2UT");
 });
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-function fetchWithAwait(stopID) {
+function stopIDToFirst5Buses(stopID) {
     return __awaiter(this, void 0, void 0, function* () {
         const myUrl = "https://api.tfl.gov.uk/StopPoint/" + stopID + "/Arrivals";
         const response = yield fetch(myUrl);
         const data = yield response.json();
         // deal with JSON response
-        console.log(data);
+        const busesArray = [];
+        const numOfBuses = Math.min(5, data.length);
+        for (let i = 0; i < numOfBuses; i++) {
+            busesArray.push(data[i].lineName);
+        }
+        return busesArray;
+    });
+}
+function postCodeToLongLat(postcode) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const myUrl = "https://api.postcodes.io/postcodes/" + postcode;
+        const response = yield fetch(myUrl);
+        const data = yield response.json();
+        return [data.result.longitude, data.result.latitude];
+    });
+}
+function longLatToBusStopIds(longLat) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const myUrl = "https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&lat=" + longLat[1] + "&lon=" + longLat[0];
+        const response = yield fetch(myUrl);
+        const data = yield response.json();
+        // deal with JSON response
+        const busIds = [];
+        const numOfStops = Math.min(2, data.stopPoints.length);
+        for (let i = 0; i < numOfStops; i++) {
+            busIds.push(data.stopPoints[i].naptanId);
+        }
+        return busIds;
+    });
+}
+function postCodeToFirst5BusesAt2NearestBusStops(postcode) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const longLat = yield postCodeToLongLat(postcode);
+        const busStopIds = yield longLatToBusStopIds(longLat);
+        let buses = [];
+        for (let i = 0; i < busStopIds.length; i++) {
+            let temp = yield stopIDToFirst5Buses(busStopIds[i]);
+            buses = buses.concat(temp);
+            console.log(temp);
+        }
+        console.log(buses);
     });
 }
--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,7 @@ const port = 3000;
 app.get('/', (req,res) =>{
     res.send("Hello I am your express server :)");
     const name: string = req.query.name as string;
-    fetchWithAwait(name);
-
+    postCodeToFirst5BusesAt2NearestBusStops("SW15 2UT")
 });
 
 app.listen(port, () =>{
@@ -16,10 +15,50 @@ app.listen(port, () =>{
 })
 
 
-async function fetchWithAwait(stopID : string): Promise<void> {
+async function stopIDToFirst5Buses(stopID : string): Promise<string[]> {
     const  myUrl : string = "https://api.tfl.gov.uk/StopPoint/" + stopID + "/Arrivals";
     const response = await fetch(myUrl);
     const data = await response.json();
     // deal with JSON response
-    console.log(stopID);
+    const busesArray = []
+    const numOfBuses = Math.min(5, data.length)
+    for (let i = 0 ; i < numOfBuses;  i ++){
+        busesArray.push(data[i].lineName)
+    }
+    return busesArray
+
+
   }
+
+async function postCodeToLongLat(postcode : string) : Promise<string[]>  {
+    const myUrl : string = "https://api.postcodes.io/postcodes/" + postcode ;
+    const response = await fetch(myUrl);
+    const data = await response.json();
+    return [data.result.longitude , data.result.latitude];
+}
+
+async function longLatToBusStopIds(longLat : string[]) : Promise<string[]> {
+    const  myUrl : string = "https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&lat=" + longLat[1] + "&lon=" + longLat[0];
+    const response = await fetch(myUrl);
+    const data = await response.json();
+    // deal with JSON response
+    const busIds = []
+    const numOfStops = Math.min(2, data.stopPoints.length)
+    for (let i = 0 ; i < numOfStops;  i ++){
+        busIds.push(data.stopPoints[i].naptanId);
+    }
+    return busIds
+}
+
+async function  postCodeToFirst5BusesAt2NearestBusStops(postcode : string) {
+    const longLat = await postCodeToLongLat(postcode)
+    const busStopIds: string[] = await longLatToBusStopIds(longLat)
+    let buses: string[] = []
+    
+    for (let i = 0; i < busStopIds.length; i++) {
+        let temp = await stopIDToFirst5Buses(busStopIds[i])
+        buses = buses.concat(temp)
+        console.log(temp)
+    }
+    console.log(buses)
+}
